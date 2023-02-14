d03d328b0bd9a0e8f5c571acc6dad3f9322ce524
Nishant Joshi <nishant.joshi@juspay.in>
2023-02-14 11:50:22 +0530

    fix: mapping bug fix

--- lua/pre-mappings.lua
+++ lua/pre-mappings.lua
@@ -4,11 +4,11 @@ vim.api.nvim_set_keymap('n', '<c-t>', '<cmd>ToggleTerm<cr>', {})
 vim.api.nvim_set_keymap('n', '<c-/>', '<cmd>gcc<cr>', {})
 
 
-vim.api.nvim_set_keymap('n', 'gpd', [[<cmd>lua require('goto-preview').goto_preview_definition()]], {})
-vim.api.nvim_set_keymap('n', 'gpt', [[<cmd>lua require('goto-preview').goto_preview_type_definition()]], {})
-vim.api.nvim_set_keymap('n', 'gpi', [[<cmd>lua require('goto-preview').goto_preview_implementation()]], {})
-vim.api.nvim_set_keymap('n', 'gP', [[<cmd>lua require('goto-preview').close_all_win()]], {})
-vim.api.nvim_set_keymap('n', 'gpr', [[<cmd>lua require('goto-preview').goto_preview_references()]], {})
+vim.api.nvim_set_keymap('n', 'gpd', [[<cmd>lua require('goto-preview').goto_preview_definition()<cr>]], {})
+vim.api.nvim_set_keymap('n', 'gpt', [[<cmd>lua require('goto-preview').goto_preview_type_definition()<cr>]], {})
+vim.api.nvim_set_keymap('n', 'gpi', [[<cmd>lua require('goto-preview').goto_preview_implementation()<cr>]], {})
+vim.api.nvim_set_keymap('n', 'gP', [[<cmd>lua require('goto-preview').close_all_win()<cr>]], {})
+vim.api.nvim_set_keymap('n', 'gpr', [[<cmd>lua require('goto-preview').goto_preview_references()<cr>]], {})
 
 vim.api.nvim_set_keymap('n', '<c-s>', [[<cmd>lua vim.lsp.buf.format { async = true }<cr>]], {})
 vim.api.nvim_set_keymap('n', '<c-.>', [[]], {})
