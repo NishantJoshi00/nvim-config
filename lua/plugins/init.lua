b24bcf80201a1446d6993e453defd0d7a7378cd7
Nishant Joshi <nishantjosh@pm.me>
2023-04-16 02:00:41 +0530

    chore: minor changes

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -202,7 +202,7 @@ return {
           header = ascii.art.text.neovim.sharp,
           shortcut = {
             { desc = '[  Github]',         group = 'DashboardShortCut' },
-            { desc = '[  NishantJoshi00]', group = 'DashboardShortCut',  action = 'lua vim.g.quote_me()', key = 'i' },
+            { desc = '[  NishantJoshi00]', group = 'DashboardShortCut', action = 'lua vim.g.quote_me()', key = 'i' },
           },
           footer = {
             '',
@@ -327,5 +327,43 @@ return {
     dependencies = {
       "MunifTanjim/nui.nvim"
     }
-  }
+  },
+  {
+    enabled = false,
+    "folke/noice.nvim",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "rcarriga/nvim-notify",
+    },
+    config = function()
+      require("noice").setup({
+        lsp = {
+          -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+          override = {
+            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+            ["vim.lsp.util.stylize_markdown"] = true,
+            ["cmp.entry.get_documentation"] = true,
+          },
+        },
+        -- you can enable a preset for easier configuration
+        presets = {
+          bottom_search = true,          -- use a classic bottom cmdline for search
+          command_palette = true,        -- position the cmdline and popupmenu together
+          long_message_to_split = false, -- long messages will be sent to a split
+          inc_rename = false,            -- enables an input dialog for inc-rename.nvim
+          lsp_doc_border = false,        -- add a border to hover docs and signature help
+        },
+      })
+    end
+  },
+  {
+    "folke/twilight.nvim",
+    config = function()
+      require("twilight").setup {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      }
+    end
+  },
 }
