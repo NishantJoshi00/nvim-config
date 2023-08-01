32ea31ce68ed34620f6b9b30414ee392b523981c
Nishant Joshi <nishant.joshi@juspay.in>
2023-08-01 18:17:30 +0530

    chore: add serde and error

--- custom_ffi/src/lib.rs
+++ custom_ffi/src/lib.rs
@@ -1,3 +1,4 @@
+use anyhow::anyhow;
 use nvim_oxi as oxi;
 use oxi::{Dictionary, Function};
 
@@ -5,13 +6,19 @@ type Result<T> = std::result::Result<T, FError>;
 
 #[derive(thiserror::Error, Debug)]
 enum FError {
-    // #[error(transparent)]
+    #[error(transparent)]
+    ReqwestError(#[from] reqwest::Error), // #[error(transparent)]
+    #[error(transparent)]
+    Others(#[from] anyhow::Error),
 }
 #[oxi::module]
 fn custom_ffi() -> oxi::Result<Dictionary> {
-    Ok(Dictionary::from_iter([("hi", Function::from_fn(hello))]))
+    Ok(Dictionary::from_iter([
+        ("hi", Function::from_fn(hello)),
+    ]))
 }
 
 fn hello(_: ()) -> Result<String> {
     Ok("Hello, World!".to_string())
 }
+
