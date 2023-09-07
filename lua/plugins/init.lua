8ced3aa06a97d800c67d3c1a2d8aeb5381be0ef8
Nishant Joshi <nishantjo.12@gmail.com>
2023-09-07 23:35:01 +0530

    move gitsigns configuration to seperate file

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,368 +1,366 @@
 return {
-	{
-		"tiagovla/tokyodark.nvim",
-		config = require("plugins.config.themes"),
-		dependencies = {
-			"catppuccin/nvim",
-			"rebelot/kanagawa.nvim",
-			"kvrohit/mellow.nvim",
-			"savq/melange-nvim",
-			"Alexis12119/nightly.nvim",
-		},
-	},
-	{ "rebelot/kanagawa.nvim" },
-	{
-		"lewis6991/gitsigns.nvim",
-		config = function()
-			require("gitsigns").setup()
-		end,
-	},
-	{ "cohama/lexima.vim" },
-	{ "preservim/vim-markdown" },
-	{ "neovim/nvim-lspconfig" },
-	{ "jose-elias-alvarez/null-ls.nvim" },
-	{ "mfussenegger/nvim-dap" },
-	{
-		"rcarriga/nvim-dap-ui",
-		config = function()
-			vim.cmd([[vnoremap <c-;> <Cmd>lua require("dapui").eval()<CR>]])
-		end,
-		dependencies = {
-			"jay-babu/mason-nvim-dap.nvim",
-		},
-	},
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		dependencies = {
-			"neovim/nvim-lspconfig",
-			"hrsh7th/cmp-nvim-lsp",
-			"simrat39/rust-tools.nvim",
-			"williamboman/mason.nvim",
-			"folke/neodev.nvim",
-			"VidocqH/lsp-lens.nvim",
-		},
-		config = require("plugins.config.mason-lspconfig"),
-	},
-	{
-		"VidocqH/lsp-lens.nvim",
-		config = function()
-			require("lsp-lens").setup({})
-		end,
-	},
-	{
-		"jay-babu/mason-null-ls.nvim",
-		dependencies = {
-			"williamboman/mason.nvim",
-			"jose-elias-alvarez/null-ls.nvim",
-			"williamboman/mason-lspconfig.nvim",
-		},
-		config = require("plugins.config.mason-null-ls"),
-	},
-	{
-		"jay-babu/mason-nvim-dap.nvim",
-		config = function()
-			require("plugins.config.nvim-dap")()
-			require("mason-nvim-dap").setup()
-		end,
-		dependencies = {
-			"williamboman/mason.nvim",
-			"mfussenegger/nvim-dap",
-		},
-	},
-	{ "nvim-lua/plenary.nvim" },
-	{ "nvim-telescope/telescope.nvim" },
-	{
-		"nvim-tree/nvim-tree.lua",
-		config = require("plugins.config.nvim-tree"),
-	},
-	{ "nvim-tree/nvim-web-devicons", dependencies = { "nvim-tree/nvim-tree.lua" } },
-	{
-		"folke/which-key.nvim",
-		config = function()
-			require("which-key").setup()
-		end,
-	},
-	{ "ryanoasis/vim-devicons" },
+  {
+    "tiagovla/tokyodark.nvim",
+    config = require("plugins.config.themes"),
+    dependencies = {
+      "catppuccin/nvim",
+      "rebelot/kanagawa.nvim",
+      "kvrohit/mellow.nvim",
+      "savq/melange-nvim",
+      "Alexis12119/nightly.nvim",
+    },
+  },
+  { "rebelot/kanagawa.nvim" },
+  {
+    "lewis6991/gitsigns.nvim",
+    config = require("plugins.config.gitsigns"),
+  },
+  { "cohama/lexima.vim" },
+  { "preservim/vim-markdown" },
+  { "neovim/nvim-lspconfig" },
+  { "jose-elias-alvarez/null-ls.nvim" },
+  { "mfussenegger/nvim-dap" },
+  {
+    "rcarriga/nvim-dap-ui",
+    config = function()
+      vim.cmd([[vnoremap <c-;> <Cmd>lua require("dapui").eval()<CR>]])
+    end,
+    dependencies = {
+      "jay-babu/mason-nvim-dap.nvim",
+    },
+  },
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = {
+      "neovim/nvim-lspconfig",
+      "hrsh7th/cmp-nvim-lsp",
+      "simrat39/rust-tools.nvim",
+      "williamboman/mason.nvim",
+      "folke/neodev.nvim",
+      "VidocqH/lsp-lens.nvim",
+    },
+    config = require("plugins.config.mason-lspconfig"),
+  },
+  {
+    "VidocqH/lsp-lens.nvim",
+    config = function()
+      require("lsp-lens").setup({})
+    end,
+  },
+  {
+    "jay-babu/mason-null-ls.nvim",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "jose-elias-alvarez/null-ls.nvim",
+      "williamboman/mason-lspconfig.nvim",
+    },
+    config = require("plugins.config.mason-null-ls"),
+  },
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    config = function()
+      require("plugins.config.nvim-dap")()
+      require("mason-nvim-dap").setup()
+    end,
+    dependencies = {
+      "williamboman/mason.nvim",
+      "mfussenegger/nvim-dap",
+    },
+  },
+  { "nvim-lua/plenary.nvim" },
+  { "nvim-telescope/telescope.nvim" },
+  {
+    "nvim-tree/nvim-tree.lua",
+    config = require("plugins.config.nvim-tree"),
+  },
+  { "nvim-tree/nvim-web-devicons", dependencies = { "nvim-tree/nvim-tree.lua" } },
+  {
+    "folke/which-key.nvim",
+    config = function()
+      require("which-key").setup()
+    end,
+  },
+  { "ryanoasis/vim-devicons" },
 
-	{ "hrsh7th/cmp-nvim-lsp" },
-	{ "hrsh7th/cmp-buffer" },
-	{ "hrsh7th/cmp-path" },
-	{ "hrsh7th/cmp-cmdline" },
-	{ "hrsh7th/cmp-vsnip" },
-	{ "hrsh7th/vim-vsnip" },
-	{
-		"hrsh7th/nvim-cmp",
-		dependencies = {
-			"hrsh7th/cmp-nvim-lsp",
-			"hrsh7th/cmp-buffer",
-			"hrsh7th/cmp-path",
-			"hrsh7th/cmp-cmdline",
-			"hrsh7th/cmp-vsnip",
-			"hrsh7th/vim-vsnip",
-		},
-		config = require("plugins.config.nvim-cmp"),
-	},
-	{ "simrat39/rust-tools.nvim" },
-	{
-		"tpope/vim-commentary",
-		config = function()
-			vim.api.nvim_set_keymap("n", "<c-/>", "gcc", { desc = "Comment Code" })
-			vim.api.nvim_set_keymap("v", "<c-/>", "gcc", { desc = "Comment Code" })
-		end,
-	},
-	{
-		"nvim-treesitter/nvim-treesitter",
-		config = require("plugins.config.nvim-treesitter"),
-		dependencies = {
-			"neovim/nvim-lspconfig",
-			"nvim-treesitter/playground",
-		},
-	},
+  { "hrsh7th/cmp-nvim-lsp" },
+  { "hrsh7th/cmp-buffer" },
+  { "hrsh7th/cmp-path" },
+  { "hrsh7th/cmp-cmdline" },
+  { "hrsh7th/cmp-vsnip" },
+  { "hrsh7th/vim-vsnip" },
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
+      "hrsh7th/cmp-vsnip",
+      "hrsh7th/vim-vsnip",
+    },
+    config = require("plugins.config.nvim-cmp"),
+  },
+  { "simrat39/rust-tools.nvim" },
+  {
+    "tpope/vim-commentary",
+    config = function()
+      vim.api.nvim_set_keymap("n", "<c-/>", "gcc", { desc = "Comment Code" })
+      vim.api.nvim_set_keymap("v", "<c-/>", "gcc", { desc = "Comment Code" })
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    config = require("plugins.config.nvim-treesitter"),
+    dependencies = {
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/playground",
+    },
+  },
 
-	{
-		"glepnir/dashboard-nvim",
-		event = "VimEnter",
-		dependencies = {
-			"MaximilianLloyd/ascii.nvim",
-		},
-		config = require("plugins.config.dashboard-nvim"),
-	},
-	-- { "ggandor/lightspeed.nvim" },
-	{
-		"folke/flash.nvim",
-		event = "VeryLazy",
-		opts = {},
-		keys = require("plugins.config.flash").key,
-	},
-	{
-		"akinsho/bufferline.nvim",
-		config = function()
-			require("bufferline").setup()
-		end,
-	},
-	{ "akinsho/toggleterm.nvim", config = require("plugins.config.toggleterm") },
-	{
-		"ellisonleao/glow.nvim",
-		config = function()
-			require("glow").setup({})
-		end,
-		enabled = function()
-			return not require("functions").disabled_on({ "win32" })
-		end,
-	},
-	{
-		"gelguy/wilder.nvim",
-		config = function()
-			require("wilder").setup({ modes = { ":", "/", "?" } })
-		end,
-	},
-	{
-		"rcarriga/nvim-notify",
-		config = function()
-			require("notify").setup({
-				render = "compact",
-			})
+  {
+    "glepnir/dashboard-nvim",
+    event = "VimEnter",
+    dependencies = {
+      "MaximilianLloyd/ascii.nvim",
+    },
+    config = require("plugins.config.dashboard-nvim"),
+  },
+  -- { "ggandor/lightspeed.nvim" },
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    opts = {},
+    keys = require("plugins.config.flash").key,
+  },
+  {
+    "akinsho/bufferline.nvim",
+    config = function()
+      require("bufferline").setup()
+    end,
+  },
+  { "akinsho/toggleterm.nvim", config = require("plugins.config.toggleterm") },
+  {
+    "ellisonleao/glow.nvim",
+    config = function()
+      require("glow").setup({})
+    end,
+    enabled = function()
+      return not require("functions").disabled_on({ "win32" })
+    end,
+  },
+  {
+    "gelguy/wilder.nvim",
+    config = function()
+      require("wilder").setup({ modes = { ":", "/", "?" } })
+    end,
+  },
+  {
+    "rcarriga/nvim-notify",
+    config = function()
+      require("notify").setup({
+        render = "compact",
+      })
 
-			vim.notify = require("notify")
+      vim.notify = require("notify")
 
-			require("functions").quoter()
-		end,
-	},
-	{
-		"rmagatti/goto-preview",
-		config = require("plugins.config.goto-preview"),
-	},
+      require("functions").quoter()
+    end,
+  },
+  {
+    "rmagatti/goto-preview",
+    config = require("plugins.config.goto-preview"),
+  },
 
-	{
-		"lukas-reineke/indent-blankline.nvim",
-		config = function()
-			require("indent_blankline").setup({
-				show_current_context = true,
-				show_current_context_start = true,
-			})
-		end,
-		dependencies = {
-			"nvim-treesitter/nvim-treesitter",
-		},
-	},
-	{ "airblade/vim-gitgutter" },
-	{
-		"nvim-lualine/lualine.nvim",
-		dependencies = {
-			"nvim-tree/nvim-web-devicons",
-		},
-		config = require("plugins.config.lualine"),
-	},
-	{
-		dependencies = {
-			"neovim/nvim-lspconfig",
-		},
-		"j-hui/fidget.nvim",
-		tag = "legacy",
-		event = "LspAttach",
-		config = function()
-			require("fidget").setup({
-				text = {
-					spinner = "dots",
-				},
-			})
-		end,
-	},
-	{
-		-- Notion Integration for NeoVim
-		"chrsm/impulse.nvim",
-		enabled = false,
-		config = function()
-			require("impulse").setup()
-		end,
-	},
-	{
-		"MunifTanjim/nui.nvim",
-		config = function() end,
-	},
-	{
-		"MaximilianLloyd/ascii.nvim",
-		dependencies = {
-			"MunifTanjim/nui.nvim",
-		},
-	},
-	{
-		enabled = false,
-		"folke/noice.nvim",
-		dependencies = {
-			"MunifTanjim/nui.nvim",
-			"rcarriga/nvim-notify",
-		},
-		config = require("plugins.config.noice"),
-	},
-	{
-		enabled = false,
-		"folke/twilight.nvim",
-		config = function()
-			require("twilight").setup({
-				-- your configuration comes here
-				-- or leave it empty to use the default settings
-				-- refer to the configuration section below
-			})
-		end,
-	},
-	{
-		"saecki/crates.nvim",
-		requires = { "nvim-lua/plenary.nvim" },
-		config = function()
-			require("crates").setup()
-		end,
-	},
-	{
-		"folke/persistence.nvim",
-		event = "BufReadPre", -- this will only start session saving when an actual file was opened
-		module = "persistence",
-		config = function()
-			require("persistence").setup()
-		end,
-	},
-	{
-		"samueljoli/hurl.nvim",
-		config = function()
-			require("hurl").setup()
-		end,
-	},
-	{
-		-- enabled = false,
-		"poljar/typos.nvim",
-		config = function()
-			require("typos").setup()
-		end,
-	},
-	{
-		"sindrets/diffview.nvim",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-		},
-	},
-	{
-		"edluffy/hologram.nvim",
-		enabled = function()
-			return not require("functions").disabled_on({ "win32" })
-		end,
-		config = function()
-			require("hologram").setup({})
-		end,
-	},
-	{
-		"giusgad/pets.nvim",
-		enabled = function()
-			return not require("functions").disabled_on({ "win32" })
-		end,
-		dependencies = {
-			"edluffy/hologram.nvim",
-			"MunifTanjim/nui.nvim",
-		},
-		config = function()
-			require("pets").setup({})
-		end,
-	},
-	{
-		"ray-x/lsp_signature.nvim",
-		config = function()
-			require("lsp_signature").setup()
-		end,
-		dependencies = {
-			"neovim/nvim-lspconfig",
-		},
-	},
-	{
-		"https://git.sr.ht/~whynothugo/lsp_lines.nvim",
-		dependencies = {
-			"neovim/nvim-lspconfig",
-		},
-		config = function()
-			require("lsp_lines").setup({})
-			vim.diagnostic.config({ virtual_lines = false })
-		end,
-	},
-	{
-		enabled = false,
-		"jbyuki/instant.nvim", -- This doesn't work as expected
-		config = function()
-			vim.cmd([[let g:instant_username = "nishant"]])
-		end,
-	},
-	{
-		"gyim/vim-boxdraw",
-	},
-	{ "ckipp01/nvim-jenkinsfile-linter", dependencies = { "nvim-lua/plenary.nvim" } },
-	{ "folke/neodev.nvim", opts = {} },
-	{
-		"stevearc/dressing.nvim",
-		opts = {},
-		config = require("plugins.config.dressing"),
-	},
-	{
-		"stevearc/overseer.nvim",
-		opts = {},
-		config = require("plugins.config.overseer"),
-	},
-	{
-		"stevearc/oil.nvim",
-		opts = {},
-		-- Optional dependencies
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		config = require("plugins.config.oil"),
-	},
-	{
-		"willothy/flatten.nvim",
-		config = true,
-		-- or pass configuration with
-		-- opts = {  }
-		-- Ensure that it runs first to minimize delay when opening file from terminal
-		lazy = false,
-		priority = 1001,
-	},
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    config = function()
+      require("indent_blankline").setup({
+        show_current_context = true,
+        show_current_context_start = true,
+      })
+    end,
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+    },
+  },
+  { "airblade/vim-gitgutter" },
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = require("plugins.config.lualine"),
+  },
+  {
+    dependencies = {
+      "neovim/nvim-lspconfig",
+    },
+    "j-hui/fidget.nvim",
+    tag = "legacy",
+    event = "LspAttach",
+    config = function()
+      require("fidget").setup({
+        text = {
+          spinner = "dots",
+        },
+      })
+    end,
+  },
+  {
+    -- Notion Integration for NeoVim
+    "chrsm/impulse.nvim",
+    enabled = false,
+    config = function()
+      require("impulse").setup()
+    end,
+  },
+  {
+    "MunifTanjim/nui.nvim",
+    config = function() end,
+  },
+  {
+    "MaximilianLloyd/ascii.nvim",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+    },
+  },
+  {
+    enabled = false,
+    "folke/noice.nvim",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "rcarriga/nvim-notify",
+    },
+    config = require("plugins.config.noice"),
+  },
+  {
+    enabled = false,
+    "folke/twilight.nvim",
+    config = function()
+      require("twilight").setup({
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      })
+    end,
+  },
+  {
+    "saecki/crates.nvim",
+    requires = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("crates").setup()
+    end,
+  },
+  {
+    "folke/persistence.nvim",
+    event = "BufReadPre", -- this will only start session saving when an actual file was opened
+    module = "persistence",
+    config = function()
+      require("persistence").setup()
+    end,
+  },
+  {
+    "samueljoli/hurl.nvim",
+    config = function()
+      require("hurl").setup()
+    end,
+  },
+  {
+    -- enabled = false,
+    "poljar/typos.nvim",
+    config = function()
+      require("typos").setup()
+    end,
+  },
+  {
+    "sindrets/diffview.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+  },
+  {
+    "edluffy/hologram.nvim",
+    enabled = function()
+      return not require("functions").disabled_on({ "win32" })
+    end,
+    config = function()
+      require("hologram").setup({})
+    end,
+  },
+  {
+    "giusgad/pets.nvim",
+    enabled = function()
+      return not require("functions").disabled_on({ "win32" })
+    end,
+    dependencies = {
+      "edluffy/hologram.nvim",
+      "MunifTanjim/nui.nvim",
+    },
+    config = function()
+      require("pets").setup({})
+    end,
+  },
+  {
+    "ray-x/lsp_signature.nvim",
+    config = function()
+      require("lsp_signature").setup()
+    end,
+    dependencies = {
+      "neovim/nvim-lspconfig",
+    },
+  },
+  {
+    "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+    dependencies = {
+      "neovim/nvim-lspconfig",
+    },
+    config = function()
+      require("lsp_lines").setup({})
+      vim.diagnostic.config({ virtual_lines = false })
+    end,
+  },
+  {
+    enabled = false,
+    "jbyuki/instant.nvim", -- This doesn't work as expected
+    config = function()
+      vim.cmd([[let g:instant_username = "nishant"]])
+    end,
+  },
+  {
+    "gyim/vim-boxdraw",
+  },
+  { "ckipp01/nvim-jenkinsfile-linter", dependencies = { "nvim-lua/plenary.nvim" } },
+  { "folke/neodev.nvim",               opts = {} },
+  {
+    "stevearc/dressing.nvim",
+    opts = {},
+    config = require("plugins.config.dressing"),
+  },
+  {
+    "stevearc/overseer.nvim",
+    opts = {},
+    config = require("plugins.config.overseer"),
+  },
+  {
+    "stevearc/oil.nvim",
+    opts = {},
+    -- Optional dependencies
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = require("plugins.config.oil"),
+  },
+  {
+    "willothy/flatten.nvim",
+    config = true,
+    -- or pass configuration with
+    -- opts = {  }
+    -- Ensure that it runs first to minimize delay when opening file from terminal
+    lazy = false,
+    priority = 1001,
+  },
 }
--- lua/post-mappings.lua
+++ lua/post-mappings.lua
@@ -6,17 +6,17 @@ local wk = require("which-key")
 vim.api.nvim_set_keymap("n", "<leader>n", [[<cmd>NvimTreeFocus<cr>]], { desc = "Focus Nerd Tree" })
 vim.api.nvim_set_keymap("n", "<leader>e", [[<cmd>NvimTreeToggle<cr>]], { desc = "Toggle Nerd Tree" })
 vim.keymap.set("n", "<c-e>", function()
-	local api = require("nvim-tree.api")
-	api.tree.toggle({ focus = false })
+  local api = require("nvim-tree.api")
+  api.tree.toggle({ focus = false })
 end, { desc = "Toggle Nerd Tree" })
 
 -- Lsp
 vim.api.nvim_set_keymap("n", "<leader>I", [[<cmd>lua vim.lsp.buf.hover()<cr>]], { desc = "View function signature" })
 vim.api.nvim_set_keymap(
-	"n",
-	"<leader>K",
-	[[<cmd>lua vim.lsp.buf.implementation()<cr>]],
-	{ desc = "View implementations" }
+  "n",
+  "<leader>K",
+  [[<cmd>lua vim.lsp.buf.implementation()<cr>]],
+  { desc = "View implementations" }
 )
 vim.api.nvim_set_keymap("n", "<leader>D", [[<cmd>lua vim.lsp.buf.definition()<cr>]], { desc = "View Definition" })
 -- Markdown specific
@@ -29,68 +29,68 @@ vim.api.nvim_set_keymap("n", "<leader>bl", [[<cmd>bnext<cr>]], { desc = "Next Bu
 vim.api.nvim_set_keymap("n", "<leader>\\", [[<cmd>lua vim.g.quote_me()<cr>]], { desc = "Quote Stuff" })
 -- Session specific
 vim.api.nvim_set_keymap(
-	"n",
-	"<leader>sc",
-	[[<cmd>lua require('persistence').load()<cr>]],
-	{ desc = "Load Session from Current Directory" }
+  "n",
+  "<leader>sc",
+  [[<cmd>lua require('persistence').load()<cr>]],
+  { desc = "Load Session from Current Directory" }
 )
 vim.api.nvim_set_keymap(
-	"n",
-	"<leader>sl",
-	[[<cmd>lua require('persistence').load({ last = true })<cr>]],
-	{ desc = "Load last session" }
+  "n",
+  "<leader>sl",
+  [[<cmd>lua require('persistence').load({ last = true })<cr>]],
+  { desc = "Load last session" }
 )
 vim.api.nvim_set_keymap(
-	"n",
-	"<leader>sq",
-	[[<cmd>lua require('persistence').stop()<cr>]],
-	{ desc = "Stop Session Recording" }
+  "n",
+  "<leader>sq",
+  [[<cmd>lua require('persistence').stop()<cr>]],
+  { desc = "Stop Session Recording" }
 )
 
 -- Telescope mappings
 vim.api.nvim_set_keymap(
-	"n",
-	"<leader>ff",
-	[[<cmd>lua require('telescope.builtin').find_files()<cr>]],
-	{ desc = "Find Files" }
+  "n",
+  "<leader>ff",
+  [[<cmd>lua require('telescope.builtin').find_files()<cr>]],
+  { desc = "Find Files" }
 )
 vim.api.nvim_set_keymap(
-	"n",
-	"<leader>fg",
-	[[<cmd>lua require('telescope.builtin').live_grep()<cr>]],
-	{ desc = "Live Grep" }
+  "n",
+  "<leader>fg",
+  [[<cmd>lua require('telescope.builtin').live_grep()<cr>]],
+  { desc = "Live Grep" }
 )
 vim.api.nvim_set_keymap(
-	"n",
-	"<leader>fb",
-	[[<cmd>lua require('telescope.builtin').buffers()<cr>]],
-	{ desc = "Buffers" }
+  "n",
+  "<leader>fb",
+  [[<cmd>lua require('telescope.builtin').buffers()<cr>]],
+  { desc = "Buffers" }
 )
 vim.api.nvim_set_keymap(
-	"n",
-	"<leader>fh",
-	[[<cmd>lua require('telescope.builtin').help_tags()<cr>]],
-	{ desc = "Help Tags" }
+  "n",
+  "<leader>fh",
+  [[<cmd>lua require('telescope.builtin').help_tags()<cr>]],
+  { desc = "Help Tags" }
 )
 vim.api.nvim_set_keymap(
-	"n",
-	"<leader>fa",
-	[[<cmd>lua require('telescope.builtin').lsp_dynamic_workspace_symbols()<cr>]],
-	{ desc = "LSP Search" }
-)
-
-vim.api.nvim_set_keymap(
-	"n",
-	"<leader>fb",
-	[[<cmd>lua require('telescope.builtin').builtin()<cr>]],
-	{ desc = "Search Builtin" }
+  "n",
+  "<leader>fa",
+  [[<cmd>lua require('telescope.builtin').lsp_dynamic_workspace_symbols()<cr>]],
+  { desc = "LSP Search" }
 )
 
 vim.api.nvim_set_keymap(
-	"n",
-	"<leader>fs",
-	[[<cmd>lua require('telescope.builtin').spell_suggest()<cr>]],
-	{ desc = "Suggest Spellings" }
+  "n",
+  "<leader>fb",
+  [[<cmd>lua require('telescope.builtin').builtin()<cr>]],
+  { desc = "Search Builtin" }
+)
+
+vim.api.nvim_set_keymap(
+  "n",
+  "<leader>fs",
+  [[<cmd>lua require('telescope.builtin').spell_suggest()<cr>]],
+  { desc = "Suggest Spellings" }
 )
 
 -- toggle term
@@ -103,34 +103,34 @@ vim.api.nvim_set_keymap("n", "<c-t>", "<cmd>ToggleTerm<cr>", { desc = "toggle te
 vim.api.nvim_set_keymap("n", "<c-/>", "<cmd>gcc<cr>", { desc = "code commenting" })
 
 vim.api.nvim_set_keymap(
-	"n",
-	"gpd",
-	[[<cmd>lua require('goto-preview').goto_preview_definition()<cr>]],
-	{ desc = "preview definition" }
+  "n",
+  "gpd",
+  [[<cmd>lua require('goto-preview').goto_preview_definition()<cr>]],
+  { desc = "preview definition" }
 )
 vim.api.nvim_set_keymap(
-	"n",
-	"gpt",
-	[[<cmd>lua require('goto-preview').goto_preview_type_definition()<cr>]],
-	{ desc = "preview type definition" }
+  "n",
+  "gpt",
+  [[<cmd>lua require('goto-preview').goto_preview_type_definition()<cr>]],
+  { desc = "preview type definition" }
 )
 vim.api.nvim_set_keymap(
-	"n",
-	"gpi",
-	[[<cmd>lua require('goto-preview').goto_preview_implementation()<cr>]],
-	{ desc = "preview implementation" }
+  "n",
+  "gpi",
+  [[<cmd>lua require('goto-preview').goto_preview_implementation()<cr>]],
+  { desc = "preview implementation" }
 )
 vim.api.nvim_set_keymap(
-	"n",
-	"gP",
-	[[<cmd>lua require('goto-preview').close_all_win()<cr>]],
-	{ desc = "close all previews" }
+  "n",
+  "gP",
+  [[<cmd>lua require('goto-preview').close_all_win()<cr>]],
+  { desc = "close all previews" }
 )
 vim.api.nvim_set_keymap(
-	"n",
-	"gpr",
-	[[<cmd>lua require('goto-preview').goto_preview_references()<cr>]],
-	{ desc = "preview references" }
+  "n",
+  "gpr",
+  [[<cmd>lua require('goto-preview').goto_preview_references()<cr>]],
+  { desc = "preview references" }
 )
 
 vim.api.nvim_set_keymap("n", "<c-s>", [[<cmd>lua vim.lsp.buf.format { async = true }<cr>]], { desc = "format file" })
@@ -140,52 +140,52 @@ vim.api.nvim_set_keymap("n", "zz", [[<cmd>spellr<cr>]], {})
 vim.api.nvim_set_keymap("n", "<c-.>", [[<cmd>lua vim.lsp.buf.code_action()<cr>]], { desc = "code action" })
 
 vim.keymap.set("n", "<leader><c-p>", function()
-	require("functions").get_current_location(function(content)
-		vim.fn.setreg("+", content)
-	end)
+  require("functions").get_current_location(function(content)
+    vim.fn.setreg("+", content)
+  end)
 end)
 
 vim.keymap.set("n", "<leader><c-n>", function()
-	require("functions").copy_pad("copy_pad", function(content)
-		vim.fn.setreg("+", content)
-	end)
+  require("functions").copy_pad("copy_pad", function(content)
+    vim.fn.setreg("+", content)
+  end)
 end, { desc = "open copy pad" })
 
 vim.keymap.set("n", "<c-n>", function()
-	require("functions").copy_pad("scratch_pad", function(content)
-		-- Decide what to do with the content
-	end)
+  require("functions").copy_pad("scratch_pad", function(content)
+    -- Decide what to do with the content
+  end)
 end, { desc = "open scratch pad" })
 
 vim.keymap.set("n", "<c-p>", function()
-	require("functions").point_search()
+  require("functions").point_search()
 end)
 
 vim.keymap.set("n", "<leader>l", function()
-	require("lsp_lines").toggle()
+  require("lsp_lines").toggle()
 end, { desc = "Toggle lsp_lines" })
 
 vim.keymap.set("n", "'", function()
-	require("macro-recorder").repetable_macro()
+  require("macro-recorder").repetable_macro()
 end, { desc = "Repeat Macro" })
 
 vim.keymap.set("n", "<leader>sm", function()
-	require("macro-recorder").select_macro()
+  require("macro-recorder").select_macro()
 end, { desc = "Select Macro" })
 
 vim.keymap.set("n", "<leader>gb", function()
-	vim.cmd([[Gitsigns blame_line]])
+  vim.cmd([[Gitsigns blame_line]])
 end, { desc = "Git blame" })
 
 vim.g.lsp_hidden = false
 vim.keymap.set("n", "<leader>hl", function()
-	if vim.g.lsp_hidden then
-		vim.diagnostic.config({ virtual_text = true })
-		vim.g.lsp_hidden = false
-	else
-		vim.diagnostic.config({ virtual_text = false })
-		vim.g.lsp_hidden = true
-	end
+  if vim.g.lsp_hidden then
+    vim.diagnostic.config({ virtual_text = true })
+    vim.g.lsp_hidden = false
+  else
+    vim.diagnostic.config({ virtual_text = false })
+    vim.g.lsp_hidden = true
+  end
 end, { desc = "Toggle Hiding Lsp Hints" })
 
 vim.api.nvim_set_keymap("n", "<leader>hf", [[<cmd>noh<cr>]], { desc = "Hide Finds" })
