1bf451ed2495cd7467f80a171a466c33087a6673
Nishant Joshi <nishant.joshi@juspay.in>
2024-08-15 21:27:49 +0530

    chore: moving config specific keybinds

--- lua/plugins/config/mason-lspconfig.lua
+++ lua/plugins/config/mason-lspconfig.lua
@@ -4,25 +4,6 @@ return function()
   require("mason-lspconfig").setup_handlers({
     -- default handler
     function(server_name)
-      vim.api.nvim_set_keymap(
-        "n",
-        "<c-.>",
-        "<cmd>lua vim.lsp.buf.code_action()<cr>",
-        { desc = "Lsp Code Action" }
-      )
-      vim.api.nvim_set_keymap(
-        "v",
-        "<c-.>",
-        "<cmd>lua vim.lsp.buf.code_action()<cr>",
-        { desc = "Lsp Code Action" }
-      )
-      vim.keymap.set("n", "<Leader>a", vim.lsp.buf.code_action, { desc = "Lsp Code Action" })
-      vim.keymap.set("v", "<Leader>a", vim.lsp.buf.code_action, { desc = "Lsp Code Action" })
-      vim.keymap.set("n", "<Leader>q", vim.lsp.buf.hover, { desc = "Hover Code Action" })
-      vim.keymap.set("n", "<Leader>q", vim.lsp.buf.implementation, { desc = "Lsp Implementation" })
-      vim.keymap.set("n", "<Leader><c-r>", vim.lsp.buf.rename, { desc = "Lsp Rename" })
-      vim.keymap.set("n", "<Leader>R", vim.lsp.buf.references, { desc = "Lsp References" })
-
       require("lspconfig")[server_name].setup({
         capabilities = capabilities,
       })
