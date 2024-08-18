88ec753c86ea144d4eb6fa2bbfa108e9f0942184
Nishant Joshi <nishant.joshi@juspay.in>
2024-08-18 12:26:10 +0530

    chore: fix hover

--- lua/plugins/keybinds/mason-lspconfig.lua
+++ lua/plugins/keybinds/mason-lspconfig.lua
@@ -11,10 +11,10 @@ return function()
     "<cmd>lua vim.lsp.buf.code_action()<cr>",
     { desc = "Lsp Code Action" }
   )
-  vim.keymap.set("n", "<Leader>a", vim.lsp.buf.code_action, { desc = "Lsp Code Action" })
-  vim.keymap.set("v", "<Leader>a", vim.lsp.buf.code_action, { desc = "Lsp Code Action" })
-  vim.keymap.set("n", "<Leader>q", vim.lsp.buf.hover, { desc = "Hover Code Action" })
-  vim.keymap.set("n", "<Leader>q", vim.lsp.buf.implementation, { desc = "Lsp Implementation" })
-  vim.keymap.set("n", "<Leader><c-r>", vim.lsp.buf.rename, { desc = "Lsp Rename" })
-  vim.keymap.set("n", "<Leader>R", vim.lsp.buf.references, { desc = "Lsp References" })
+  lua_keymap("n", "<Leader>a", vim.lsp.buf.code_action, { desc = "Lsp Code Action" })
+  lua_keymap("v", "<Leader>a", vim.lsp.buf.code_action, { desc = "Lsp Code Action" })
+  lua_keymap("n", "<Leader>q", vim.lsp.buf.hover, { desc = "Hover Code Action" })
+  lua_keymap("n", "<Leader>Q", vim.lsp.buf.implementation, { desc = "Lsp Implementation" })
+  lua_keymap("n", "<Leader><c-r>", vim.lsp.buf.rename, { desc = "Lsp Rename" })
+  lua_keymap("n", "<Leader>R", vim.lsp.buf.references, { desc = "Lsp References" })
 end
