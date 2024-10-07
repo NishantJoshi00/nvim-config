d82af572e923f2ac4e67d7933b5121dd5972f611
Nishant Joshi <nishantjo.12@gmail.com>
2024-10-08 00:42:58 +0530

    chore: remove telemetry

--- init.lua
+++ init.lua
@@ -1,17 +1,13 @@
+vim.g.mapleader = " "
+
 require("bootstrap")
 require("theme")
 
-vim.g.mapleader = " "
 require("lazy").setup(require("plugins"))
+
 require("config")
-
-require("post-mappings")
+require("mappings")
 require("masking")
-
 require("custom")
 
 require("functions.up-to-date").check()
-
-vim.opt.rtp:prepend("~/.config/nvim/ffi")
-
-require("telemetry.keybinds")
--- telemetry/export.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-cp target/release/libtelemetry.dylib ../ffi/lua/telemetry.so
