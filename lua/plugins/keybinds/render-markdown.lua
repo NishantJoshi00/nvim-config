530aae080a15a4f2c44f559c04c10047c1b0b65e
Nishant Joshi <nishant.joshi@juspay.in>
2025-07-24 12:08:09 +0530

    chore: disable render

--- lua/plugins/keybinds/render-markdown.lua
+++ lua/plugins/keybinds/render-markdown.lua
@@ -1,3 +1,4 @@
 return function()
+    vim.cmd([[RenderMarkdown disable]])
     vim.api.nvim_set_keymap("n", "<leader>rm", [[<cmd>RenderMarkdown toggle<cr>]], { desc = "Toggle Markdown Rendering" })
 end
